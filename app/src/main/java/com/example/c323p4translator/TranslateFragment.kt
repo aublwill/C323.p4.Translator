6d001c68e9e87d8dbee10a71e239567a597d5b2f
aublwill <aublwill@iu.edu>
2023-10-03 22:47:41 -0400

    It works!

--- app/src/main/java/com/example/c323p4translator/TranslateFragment.kt
+++ app/src/main/java/com/example/c323p4translator/TranslateFragment.kt
@@ -3,23 +3,14 @@ package com.example.c323p4translator
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.c323p4translator.databinding.ActivityMainBinding
 import com.example.c323p4translator.databinding.FragmentTranslateBinding
 import com.google.mlkit.nl.translate.TranslateLanguage
-import com.google.mlkit.nl.translate.Translation
-import com.google.mlkit.nl.translate.Translator
-import com.google.mlkit.nl.translate.TranslatorOptions
-import com.google.mlkit.nl.languageid.LanguageIdentification
-import com.google.mlkit.nl.languageid.LanguageIdentificationOptions
 
 
 class TranslateFragment : Fragment() {
@@ -42,20 +33,20 @@ class TranslateFragment : Fragment() {
 
         sourceLangRG.setOnCheckedChangeListener{group, checkedId ->
             if (binding.rbEngS.isChecked)
-                viewModel.setSourceLanguage(TranslateLanguage.ENGLISH)
+                viewModel.setSourceLanguage("English")
             else if (binding.rbSpanS.isChecked)
-                viewModel.setSourceLanguage(TranslateLanguage.SPANISH)
+                viewModel.setSourceLanguage("Spanish")
             else if (binding.rbGermS.isChecked)
-                viewModel.setSourceLanguage(TranslateLanguage.GERMAN)
+                viewModel.setSourceLanguage("German")
 
         }
         targetLangRG.setOnCheckedChangeListener{group, checkedId ->
             if (binding.rbEngT.isChecked)
-                viewModel.setTargetLanguage(TranslateLanguage.ENGLISH)
+                viewModel.setTargetLanguage("English")
             else if (binding.rbSpanT.isChecked)
-                viewModel.setTargetLanguage(TranslateLanguage.SPANISH)
+                viewModel.setTargetLanguage("Spanish")
             else if (binding.rbGermT.isChecked)
-                viewModel.setTargetLanguage(TranslateLanguage.GERMAN)
+                viewModel.setTargetLanguage("German")
         }
 
        // updateScreen()
